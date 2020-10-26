10676c07dfae7a4c65439877e37d8e75bd8294a4
Fredomontero <christianmontero08@gmail.com>
2020-10-25 21:03:06 -0700

    Feat: Add yargs dependencie and add templates for add, remove, list and read notes

--- app.js
+++ app.js
@@ -1,4 +1,44 @@
 const notes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-console.log(chalk.green('success'));
+//Customize yargs version
+yargs.version('1.1.0');
+
+//Create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: () => {
+    console.log(" Adding a new note");
+  }
+});
+
+//Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a new note',
+  handler: () => {
+    console.log(" removing a note");
+  }
+});
+
+//Create list command
+yargs.command({
+  command: 'list',
+  describe: 'Listing existing notes',
+  handler: () => {
+    console.log("Listing all the notes");
+  }
+});
+
+//Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read a note',
+  handler: () => {
+    console.log("Reading a note");
+  }
+});
+
+console.log(yargs.argv); 
